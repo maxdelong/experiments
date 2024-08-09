75fbca6ec256926b99b0aed0bad61dd81ad0383c
Max DeLong <95108037+maxdelong@users.noreply.github.com>
2024-08-09 16:40:47 -0400

    Add red circle 'game'

--- tower_defense_game/src/tower_defense_game/main.py
+++ tower_defense_game/src/tower_defense_game/main.py
@@ -1,73 +1,42 @@
+# Example file showing a circle moving on screen
 import pygame
-from pygame.locals import *
-import sys
-
+import os
+# pygame setup
 pygame.init()
-vec = pygame.math.Vector2  # 2 for two dimensional
- 
-HEIGHT = 450
-WIDTH = 400
-ACC = 0.5
-FRIC = -0.12
-FPS = 60
- 
-FramePerSec = pygame.time.Clock()
- 
-displaysurface = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Game")
+screen = pygame.display.set_mode((1280, 1280))
+clock = pygame.time.Clock()
+running = True
+dt = 0
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__() 
-        self.surf = pygame.Surface((30, 30))
-        self.surf.fill((128,255,40))
-        self.rect = self.surf.get_rect()
-   
-        self.pos = vec((10, 385))
-        self.vel = vec(0,0)
-        self.acc = vec(0,0)
-    def move(self):
-        self.acc = vec(0,0)
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+background_path = './src/data/forest.png'
+surface = pygame.image.load(background_path).convert()
 
-        pressed_keys = pygame.key.get_pressed()
-                
-        if pressed_keys[K_LEFT]:
-            self.acc.x = -ACC
-        if pressed_keys[K_RIGHT]:
-            self.acc.x = ACC
-        self.acc.x += self.vel.x * FRIC
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
-        if self.pos.x > WIDTH:
-            self.pos.x = 0
-        if self.pos.x < 0:
-            self.pos.x = WIDTH
-        self.rect.midbottom = self.pos
-class platform(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.surf = pygame.Surface((WIDTH, 20))
-        self.surf.fill((255,0,0))
-        self.rect = self.surf.get_rect(center = (WIDTH/2, HEIGHT - 10))
-
-PT1 = platform()
-P1 = Player()
-
-all_sprites = pygame.sprite.Group()
-all_sprites.add(PT1)
-all_sprites.add(P1)
- 
-while True:
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-     
-    displaysurface.fill((0,0,0))
- 
-    for entity in all_sprites:
-        displaysurface.blit(entity.surf, entity.rect)
- 	
-    P1.move()
-    pygame.display.update()
-    FramePerSec.tick(FPS)
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.blit(surface, (0,0))
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+    player_pos = pygame.mouse.get_pos()
+    keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+    #     player_pos.y -= 300 * dt
+    # if keys[pygame.K_s]:
+    #     player_pos.y += 300 * dt
+    # if keys[pygame.K_a]:
+    #     player_pos.x -= 300 * dt
+    # if keys[pygame.K_d]:
+    #     player_pos.x += 300 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
